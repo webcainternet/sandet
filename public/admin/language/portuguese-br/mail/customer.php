ca1256ad8f7b7702fd9e520da620302d3b043d43
fernandofmendes <fernando.mendes@webca.com.br>
2016-11-28 16:31:55 -0200

    email template

--- public/admin/language/portuguese-br/mail/customer.php
+++ public/admin/language/portuguese-br/mail/customer.php
@@ -1,7 +1,7 @@
 <?php
 // Text
 $_['text_approve_subject']      = '%s - Sua conta foi aprovada!';
-$_['text_approve_welcome']      = 'Obrigado por se cadastrar na loja %s!';
+$_['text_approve_welcome']      = 'Obrigado por se cadastrar na %s!';
 $_['text_approve_login']        = 'Sua conta foi aprovada, agora você pode acessar sua conta utilizando seu e-mail e sua senha através de nossa loja:';
 $_['text_approve_services']     = 'Ao acessar sua conta, você poderá visualizar o histórico de seus pedidos, imprimir faturas, fazer downloads que tenha comprado, modificar as informações de sua conta e muito mais.';
 $_['text_approve_thanks']       = 'Atenciosamente,';
