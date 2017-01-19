9bc87a0ae141e80cdce2995980b740c2fa85b744
fernandofmendes <fernando.mendes@webca.com.br>
2017-01-19 08:14:42 -0200

    hello

--- public/catalog/controller/common/header.php
+++ public/catalog/controller/common/header.php
@@ -56,6 +56,8 @@ class ControllerCommonHeader extends Controller {
 			$data['text_wishlist'] = sprintf($this->language->get('text_wishlist'), (isset($this->session->data['wishlist']) ? count($this->session->data['wishlist']) : 0));
 		}
 
+		$data['customer_firstname'] = $this->customer->getFirstName();
+
 		$data['text_shopping_cart'] = $this->language->get('text_shopping_cart');
 		$data['text_logged'] = sprintf($this->language->get('text_logged'), $this->url->link('account/account', '', 'SSL'), $this->customer->getFirstName(), $this->url->link('account/logout', '', 'SSL'));
 
