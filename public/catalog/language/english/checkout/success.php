f43ee97b7c3838841436e23f4985db6a93584fa1
fernandofmendes <fernando.mendes@webca.com.br>
2017-02-06 18:14:34 -0200

    correct atendimento

--- public/catalog/language/english/checkout/success.php
+++ public/catalog/language/english/checkout/success.php
@@ -6,5 +6,5 @@ $_['heading_title']        = 'Pedido cadastrado.';
 $_['text_basket']          = 'Carrinho de compras';
 $_['text_checkout']        = 'Finalizar pedido';
 $_['text_success']         = 'Confirmação';
-$_['text_customer']        = '<p>O seu pedido foi cadastrado em nossa loja.</p><p>Você pode ver o histórico dos seus pedidos através de sua <a href="%s">conta</a>, acessando o menu <a href="%s">histórico de pedidos</a>.</p><p>Entre em contato com nosso <a href="%s">atendimento</a> caso tenha dúvidas.</p><p>Obrigado por comprar em nossa loja.</p>';
-$_['text_guest']           = '<p>O seu pedido foi cadastrado em nossa loja.</p><p>Entre em contato com nosso <a href="%s">atendimento</a> caso tenha dúvidas.</p><p>Obrigado por comprar em nossa loja.</p>';
+$_['text_customer']        = '<p>O seu pedido foi cadastrado em nossa loja.</p><p>Você pode ver o histórico dos seus pedidos através de sua <a href="%s">conta</a>, acessando o menu <a href="%s">histórico de pedidos</a>.</p><p>Entre em contato com nosso <a href="/index.php?route=information/contact">atendimento</a> caso tenha dúvidas.</p><p>Obrigado por comprar em nossa loja.</p>';
+$_['text_guest']           = '<p>O seu pedido foi cadastrado em nossa loja.</p><p>Entre em contato com nosso <a href="/index.php?route=information/contact">atendimento</a> caso tenha dúvidas.</p><p>Obrigado por comprar em nossa loja.</p>';
